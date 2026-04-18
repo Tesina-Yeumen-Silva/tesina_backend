3ce865adb5d3009bfd5d59156bdb3d711ab4a24f
Yeu <yeumensilva9@gmail.com>
2026-04-18 12:21:43 -0300

    add refresh logic

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,8 +1,18 @@
 import jwt from "jsonwebtoken";
-import type{ JwtPayload } from "../middleware/authMiddleware.js";
+import crypto from "crypto";
 
-export function signToken(payload: { userId: number; email: string; role: string }) {
-  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "7d" });
+export interface JwtPayload {
+  userId: number;
+  email: string;
+  role: string;
+}
+
+export function signAccessToken(payload: JwtPayload) {
+  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "15m" });
+}
+
+export function signRefreshToken() {
+  return crypto.randomBytes(64).toString("hex");
 }
 
 export function verifyToken(token: string): JwtPayload {
