34fb216b4f3635d64904283f2e7a7285674e48d0
Yeu <yeumensilva9@gmail.com>
2026-04-21 22:34:48 -0300

    follow apiRest standar to routes

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,9 @@ import passport from "./config/passport.js";
 import roleRouter from './routes/role.routes.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
-import reportCategroyRouter from './routes/reportCategory.routes.js'
-import reportStateRouter from './routes/reportState.routes.js'
+import reportCategoryRouter from './routes/reportCategory.routes.js';
+import reportStateRouter from './routes/reportState.routes.js';
+import reportRouter from './routes/report.routes.js';
 
 
 const app = express();
@@ -14,11 +15,14 @@ app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
-app.use("/role",roleRouter);
-app.use("/auth",authRouter);
-app.use("/user",userRouter);
-app.use("/reportCategory",reportCategroyRouter);
-app.use("/reportState",reportStateRouter);
+app.use("/roles", roleRouter);
+app.use("/auth", authRouter); 
+app.use("/users", userRouter);
+
+app.use("/reports", reportRouter);
+
+app.use("/report-categories", reportCategoryRouter); 
+app.use("/report-states", reportStateRouter);
 
 
 export default app;
