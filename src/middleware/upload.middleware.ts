8345c24a6b95b037ba82f3f50c0bab4144b8e8e5
Yeu <yeumensilva9@gmail.com>
2026-04-22 17:40:45 -0300

    upload image middleware

---
+++ src/middleware/upload.middleware.ts
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import type{ FileFilterCallback } from 'multer';
+import type { Request } from 'express';
+import { AppError } from '../utils/appError.js';
+
+const ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png', 'image/webp'];
+const MAX_FILE_SIZE_MB = 10;
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req:Request,file:Express.Multer.File,cb:FileFilterCallback) => {
+    if(ALLOWED_MIME_TYPES.includes(file.mimetype)){
+        cb(null,true);
+    }else{
+        cb(new AppError("File type not supported",400))
+    }
+}
+
+export const uploadMiddleware = multer({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize: MAX_FILE_SIZE_MB * 1024 * 1024
+    }
+});
