b9afcc9f9b7f8d23d8ec817c81cdd288f2f5d344
Yeu <yeumensilva9@gmail.com>
2026-04-27 16:32:41 -0300

    Integrate email and restore password

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,23 +1,37 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const registerLocalSchema = z.object({
-    email: z.email("El formato del correo es inválido"),
-    password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
-    name: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
-    roleId: z.coerce.number().positive().optional()
+  email: z.email("El formato del correo es inválido"),
+  password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
+  name: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
+  roleId: z.coerce.number().positive().optional(),
 });
 
 export const loginLocalSchema = z.object({
-    email: z.email("El formato del correo es inválido"),
-    password: z.string().min(1, "La contraseña es obligatoria")
+  email: z.email("El formato del correo es inválido"),
+  password: z.string().min(1, "La contraseña es obligatoria"),
 });
-
 
 export const tokenSchema = z.object({
-    refreshToken: z.string().min(1, "El refresh token es obligatorio")
+  refreshToken: z.string().min(1, "El refresh token es obligatorio"),
 });
 
+export const requestPasswordResetSchema = z.object({
+  email: z.email("El formato del correo es inválido"),
+});
+
+export const confirmPasswordResetSchema = z.object({
+  email: z.email("El formato del correo es inválido"),
+  code: z.string().min(1, "El código es obligatorio"),
+  newPassword: z.string().min(1, "La contraseña es obligatoria"),
+});
 
 export type RegisterLocalDTO = z.infer<typeof registerLocalSchema>;
 export type LoginLocalDTO = z.infer<typeof loginLocalSchema>;
 export type TokenDTO = z.infer<typeof tokenSchema>;
+export type RequestPasswordResetDTO = z.infer<
+  typeof requestPasswordResetSchema
+>;
+export type confirmPasswordResetDTO = z.infer<
+  typeof confirmPasswordResetSchema
+>;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,15 @@
 import bcrypt from "bcryptjs";
-import { prisma } from '../config/prisma.js';
+import crypto from "crypto";
+import { prisma } from "../config/prisma.js";
 import { signAccessToken, signRefreshToken } from "../utils/jwt.js";
 import { AppError } from "../utils/appError.js";
-import type { LoginLocalDTO, RegisterLocalDTO } from "../schemas/auth.schema.js";
+import type {
+  confirmPasswordResetDTO,
+  LoginLocalDTO,
+  RegisterLocalDTO,
+  RequestPasswordResetDTO,
+} from "../schemas/auth.schema.js";
+import { sendPasswordReset } from "./emailServices.js";
 
 export async function generateRefreshTokenService(userId: number) {
   const token = signRefreshToken();
@@ -40,60 +47,149 @@ export async function logoutService(refreshToken: string) {
   await prisma.refreshToken.deleteMany({ where: { token: refreshToken } });
 }
 
-export async function registerLocalService(data: RegisterLocalDTO) {
-    const { email, password, name, roleId = 1 } = data;
+export async function requestPasswordResetService(
+  data: RequestPasswordResetDTO,
+) {
+  const user = await prisma.user.findFirst({
+    where: { email: data.email, deletedAt: null },
+    include: { authProviders: true },
+  });
 
-    const existingUser = await prisma.user.findFirst({ 
-        where: { email, deletedAt: null } 
-    });
-    if (existingUser) throw new AppError("Email already registered", 409);
+  if (!user) throw new AppError("User not found", 404);
 
-    const passwordHash = await bcrypt.hash(password, 10);
+  const hasLocalProvider = user.authProviders.some(
+    (p) => p.provider === "local",
+  );
+  if (!hasLocalProvider) throw new AppError("Dont have local acount", 400);
 
-    const user = await prisma.user.create({
-        data: {
-            email,
-            name,
-            role: { connect: { id: roleId } },
-            authProviders: {
-                create: {
-                    provider: "local",
-                    passwordHash
-                }
-            }
+  const token = crypto.randomBytes(32).toString("hex");
+  const expiresAt = new Date(Date.now() + 1000 * 60 * 60);
+
+  await prisma.passwordResetToken.create({
+    data: {
+      token,
+      userId: user.id,
+      expiresAt,
+    },
+  });
+  await sendPasswordReset(user.email, token);
+}
+
+export async function confirmPasswordResetService(
+  data: confirmPasswordResetDTO,
+) {
+  const resetToken = await prisma.passwordResetToken.findFirst({
+    where: { token: data.code },
+  });
+
+  if (!resetToken) throw new AppError("Invalid Token", 400);
+  if (resetToken.expiresAt < new Date())
+    throw new AppError("Expired Token", 400);
+
+  const user = await prisma.user.findFirst({
+    where: { email: data.email, deletedAt: null },
+  });
+
+  if (!user) throw new AppError("User not found", 404);
+  if (resetToken.userId !== user.id) throw new AppError("Invalid Token", 400);
+
+  const hashedPassword = await bcrypt.hash(data.newPassword, 10);
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      authProviders: {
+        update: {
+          where: {
+            userId_provider: {
+              userId: user.id,
+              provider: "local",
+            },
+          },
+          data: { passwordHash: hashedPassword },
         },
-        include: { role: true }
-    });
+      },
+    },
+  });
 
-    const token = signAccessToken({ userId: user.id, email: user.email, role: user.role.name });
-    const refreshToken = await generateRefreshTokenService(user.id);
+  await prisma.passwordResetToken.delete({
+    where: { id: resetToken.id },
+  });
+}
 
-    return { token, refreshToken, user: { id: user.id, email: user.email, role: user.role.name } };
+export async function registerLocalService(data: RegisterLocalDTO) {
+  const { email, password, name, roleId = 1 } = data;
+
+  const existingUser = await prisma.user.findFirst({
+    where: { email, deletedAt: null },
+  });
+  if (existingUser) throw new AppError("Email already registered", 409);
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      role: { connect: { id: roleId } },
+      authProviders: {
+        create: {
+          provider: "local",
+          passwordHash,
+        },
+      },
+    },
+    include: { role: true },
+  });
+
+  const token = signAccessToken({
+    userId: user.id,
+    email: user.email,
+    role: user.role.name,
+  });
+  const refreshToken = await generateRefreshTokenService(user.id);
+
+  return {
+    token,
+    refreshToken,
+    user: { id: user.id, email: user.email, role: user.role.name },
+  };
 }
 
 export async function loginLocalService(data: LoginLocalDTO) {
-    const { email, password } = data;
+  const { email, password } = data;
 
-    const user = await prisma.user.findFirst({
-        where: { email, deletedAt: null },
-        include: {
-            role: true,
-            authProviders: { where: { provider: "local" } }
-        }
-    });
+  const user = await prisma.user.findFirst({
+    where: { email, deletedAt: null },
+    include: {
+      role: true,
+      authProviders: { where: { provider: "local" } },
+    },
+  });
 
-    if (!user || user.authProviders.length === 0) {
-        throw new AppError("Invalid email or password", 401);
-    }
-    
-    const localProvider = user.authProviders[0];
-    if (localProvider) {
-        const validPassword = await bcrypt.compare(password, localProvider.passwordHash!);
-        if (!validPassword) throw new AppError("Invalid email or password", 401);
-    }
-    
-    const token = signAccessToken({ userId: user.id, email: user.email, role: user.role.name });
-    const refreshToken = await generateRefreshTokenService(user.id);
-    
-    return { token, refreshToken, user: { id: user.id, email: user.email, role: user.role.name } };
+  if (!user || user.authProviders.length === 0) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const localProvider = user.authProviders[0];
+  if (localProvider) {
+    const validPassword = await bcrypt.compare(
+      password,
+      localProvider.passwordHash!,
+    );
+    if (!validPassword) throw new AppError("Invalid email or password", 401);
+  }
+
+  const token = signAccessToken({
+    userId: user.id,
+    email: user.email,
+    role: user.role.name,
+  });
+  const refreshToken = await generateRefreshTokenService(user.id);
+
+  return {
+    token,
+    refreshToken,
+    user: { id: user.id, email: user.email, role: user.role.name },
+  };
 }
